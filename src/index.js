03ebf3c631aaf49cca7c9051734d3a34fea26041
noiseLim <noiselim@gmail.com>
2020-12-25 16:44:45 +0300

    test redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const reducer = (state = 0, action) => {
+    switch (action.type) {
+        case 'INC':
+            return state + 1;
+        default:
+            return state;
+    }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const store = createStore(reducer);
+
+store.subscribe(() => {
+    console.log(store.getState());
+})
+
+store.dispatch({type: 'INC'});
+store.dispatch({type: 'INC'});
+store.dispatch({type: 'INC'});
