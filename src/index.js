e2a7937e3d7e8329b537eb3491dddd9f205db589
noiseLim <noiselim@gmail.com>
2020-12-26 11:25:54 +0300

    finish lesson #105 'logic redux'

--- src/index.js
+++ src/index.js
@@ -4,17 +4,40 @@ const reducer = (state = 0, action) => {
     switch (action.type) {
         case 'INC':
             return state + 1;
+        case 'DEC':
+            return state - 1;
+        case 'RND':
+            return state + action.value;
         default:
             return state;
     }
 }
 
+const inc = () => ({type: 'INC'});
+const dec = () => ({type: 'DEC'});
+const rnd = (value) => ({type: 'RND', value});
+
 const store = createStore(reducer);
 
-store.subscribe(() => {
-    console.log(store.getState());
+document.getElementById('inc').addEventListener('click', () => {
+    store.dispatch(inc());
 })
 
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
+document.getElementById('dec').addEventListener('click', () => {
+    store.dispatch(dec());
+})
+
+document.getElementById('rnd').addEventListener('click', () => {
+    const value = Math.floor(Math.random() * 10);
+    store.dispatch(rnd(value));
+})
+
+const update = () => {
+    document.getElementById('counter').textContent = store.getState();
+}
+
+store.subscribe(update);
+
+// store.dispatch({type: 'INC'});
+// store.dispatch({type: 'INC'});
+// store.dispatch({type: 'INC'});
