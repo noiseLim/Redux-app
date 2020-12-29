e7c0a955deedb1e2e3aee804189dd5ac2c32f6e2
noiseLim <noiselim@gmail.com>
2020-12-29 17:22:20 +0300

    react+redux part 1

--- src/index.js
+++ src/index.js
@@ -1,56 +1,25 @@
+import {createStore, bindActionCreators} from 'redux';
+import reducer from './reducer';
+import * as actions from './action';
 
+const store = createStore(reducer);
+const {dispatch} = store;
 
-
-
-
-
-
-
-
-
-
-
-
-// import {createStore} from 'redux';
-
-// const reducer = (state = 0, action) => {
-//     switch (action.type) {
-//         case 'INC':
-//             return state + 1;
-//         case 'DEC':
-//             return state - 1;
-//         case 'RND':
-//             return state + action.value;
-//         default:
-//             return state;
-//     }
+// const bindActionCreator = (creator, dispatch) => (...args) => {
+//     dispatch(creator(...args));
 // }
 
-// const inc = () => ({type: 'INC'});
-// const dec = () => ({type: 'DEC'});
-// const rnd = (value) => ({type: 'RND', value});
+const {inc, dec, rnd} = bindActionCreators(actions, dispatch)
 
-// const store = createStore(reducer);
+document.getElementById('inc').addEventListener('click', inc);
+document.getElementById('dec').addEventListener('click', dec);
+document.getElementById('rnd').addEventListener('click', () => {
+    const value = Math.floor(Math.random() * 10);
+    rnd(value);
+})
 
-// document.getElementById('inc').addEventListener('click', () => {
-//     store.dispatch(inc());
-// })
+const update = () => {
+    document.getElementById('counter').textContent = store.getState();
+}
 
-// document.getElementById('dec').addEventListener('click', () => {
-//     store.dispatch(dec());
-// })
-
-// document.getElementById('rnd').addEventListener('click', () => {
-//     const value = Math.floor(Math.random() * 10);
-//     store.dispatch(rnd(value));
-// })
-
-// const update = () => {
-//     document.getElementById('counter').textContent = store.getState();
-// }
-
-// store.subscribe(update);
-
-// // store.dispatch({type: 'INC'});
-// // store.dispatch({type: 'INC'});
-// // store.dispatch({type: 'INC'});
+store.subscribe(update);
