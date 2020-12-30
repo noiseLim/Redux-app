b6daccccc8394df73a1e968166f4a7428feabff5
noiseLim <noiselim@gmail.com>
2020-12-30 13:06:45 +0300

    react+redux part 2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import {createStore, bindActionCreators} from 'redux';
 import reducer from './reducer';
 import * as actions from './action';
+import Counter from './counter';
 
 const store = createStore(reducer);
 const {dispatch} = store;
@@ -11,15 +14,17 @@ const {dispatch} = store;
 
 const {inc, dec, rnd} = bindActionCreators(actions, dispatch)
 
-document.getElementById('inc').addEventListener('click', inc);
-document.getElementById('dec').addEventListener('click', dec);
-document.getElementById('rnd').addEventListener('click', () => {
-    const value = Math.floor(Math.random() * 10);
-    rnd(value);
-})
-
 const update = () => {
-    document.getElementById('counter').textContent = store.getState();
+    ReactDOM.render(<Counter
+        counter={store.getState()}
+        inc={inc}
+        dec={dec}
+        rnd={() => {
+            const value = Math.floor(Math.random() * 10);
+            rnd(value);
+        }}/>, document.getElementById('root'));
 }
+update();
 
 store.subscribe(update);
+
